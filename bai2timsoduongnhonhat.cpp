11fc83a35654d81e376bcbefda2a8b410181b0ea
Van Thu Nguyen <70945383+vanthu13@users.noreply.github.com>
2020-09-25 20:05:01 +0700

    Bai Tap Buoi 6_Van Thu

---
+++ bai2timsoduongnhonhat.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+	int n;
+	printf("Nhap n=");
+	scanf("%d",&n);
+	int arr[n];
+
+	for(int i=0;i<n;i++){
+		printf("Nhap so thu %d: ",i);
+		scanf("%d",&arr[i]);
+	}
+
+	int x=0;
+	for(int i=0;i<n;i++){
+		if(arr[i]>0){
+			x= arr[i];
+			break;
+		}
+	}
+
+	if(x==0){
+		printf("Mang ko co so duong nao ca!");
+	}else{
+		for(int i=0;i<n;i++){
+			if(arr[i]>0 && arr[i]<x){
+				x = arr[i];
+			}
+		}
+		printf("So duong nho nhat: %d",x);
+	}
+}
